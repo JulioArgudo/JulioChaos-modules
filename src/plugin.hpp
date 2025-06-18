cd2fe7aa47692c30d3702b74261d2fcf9d2a4943
Julio <argudomielgo@gmail.com>
2025-06-19 00:50:41 +0200

    Add files via upload

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -6,7 +6,9 @@ using namespace rack;
 
 // Declare the Plugin, defined in plugin.cpp
 extern Plugin* pluginInstance;
-extern Model* modelJulioChua;
 
+extern Model* modelJulioRossler;
+extern Model* modelJulioChua;
+extern Model* modelJulioLorentz84;
 // Declare each Model, defined in each module source file
 // extern Model* modelMyModule;
